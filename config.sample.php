84107f5ef00fcd9dac2a42c78ed5664d6006addf
pp19dd <pp19dd@gmail.com>
2017-04-17 16:22:38 -0400

    Config and htaccess samples

--- config.sample.php
+++ config.sample.php
@@ -3,3 +3,7 @@
 define( "CONFIG_DSN", "mysql:host=127.0.0.1;dbname=dbname;charset=utf8" );
 define( "CONFIG_USER", "user" );
 define( "CONFIG_PASS", "pass" );
+
+// no trailing slashes
+define( "CONFIG_TE", "http://localhost/timeline-editor" );
+define( "CONFIG_HOME", "http://localhost/newsletter-archive" );
